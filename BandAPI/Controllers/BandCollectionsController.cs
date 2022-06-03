660de2a812524d4aaac41c5c14089f48e758da65
Amra Dedic <amrade@maestralsolutions.com>
2022-06-03 16:15:21 +0200

    Section 13, Model binding

--- BandAPI/Controllers/BandCollectionsController.cs
+++ BandAPI/Controllers/BandCollectionsController.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
 using BandAPI.Dtos;
+using BandAPI.Helpers;
 using BandAPI.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BandAPI.Controllers
 {
@@ -17,11 +21,31 @@ namespace BandAPI.Controllers
 
         public BandCollectionsController(IBandAlbumRepository bandAlbumRepository, IMapper mapper)
         {
-            _bandAlbumRepository = bandAlbumRepository;
-            _mapper = mapper;
+            _bandAlbumRepository = bandAlbumRepository ??
+               throw new ArgumentNullException(nameof(bandAlbumRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
         }
+
+        [HttpGet("({ids})", Name = "GetBandCollection")]
+        public IActionResult GetBandCollection([FromRoute]
+               [ModelBinder(BinderType = typeof(ArrayModelBinder))]IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+                return BadRequest();
+
+            var bandEntities = _bandAlbumRepository.GetBands(ids);
+
+            if (ids.Count() != bandEntities.Count())
+                return NotFound();
+
+            var bandsToReturn = _mapper.Map<IEnumerable<BandDto>>(bandEntities);
+
+            return Ok(bandsToReturn);
+        }
+
         [HttpPost]
-        public ActionResult<List<BandDto>> CreateBandCollection([FromBody] IEnumerable<BandForCreatingDto> bandColl)
+        public ActionResult<IEnumerable<BandDto>> CreateBandCollection([FromBody] IEnumerable<BandForCreatingDto> bandColl)
         {
             var bandEntities = _mapper.Map<IEnumerable<Entities.Band>>(bandColl);
             foreach (var band in bandEntities)
@@ -30,7 +54,12 @@ namespace BandAPI.Controllers
             }
             _bandAlbumRepository.Save();
 
-            return Ok();
+
+            _bandAlbumRepository.Save();
+            var bandCollectionToReturn = _mapper.Map<IEnumerable<BandDto>>(bandEntities);
+            var IdsString = string.Join(",", bandCollectionToReturn.Select(a => a.Id));
+
+            return CreatedAtRoute("GetBandCollection", new { ids = IdsString }, bandCollectionToReturn);
         }
     }
 }
---
+++ BandAPI/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BandAPI.Helpers
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            //make sure that modelbinder works only for IEnumerable types
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+            //get value from input
+            var value=bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+
+            //check if its null
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                //null is not the value we can process, however, it is not a failed result; it can still be ienumerable type
+                //action will return bad request
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+
+            //get the type
+            var elementType = bindingContext.ModelType.GetTypeInfo().GenericTypeArguments[0];
+
+            var converter=TypeDescriptor.GetConverter(elementType);
+
+            var values = value.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => converter.ConvertFromString(x.Trim())).ToArray();
+
+            var typeValues = Array.CreateInstance(elementType, values.Length);
+            values.CopyTo(typeValues, 0);
+            bindingContext.Model = typeValues;
+
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+
+        }
+    }
+}

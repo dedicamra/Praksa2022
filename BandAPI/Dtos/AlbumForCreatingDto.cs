9402377fca446723852c8173d91d845de18156e3
Amra Dedic <amrade@maestralsolutions.com>
2022-06-03 10:34:00 +0200

    Section 9, data validation

--- BandAPI/Dtos/AlbumForCreatingDto.cs
+++ BandAPI/Dtos/AlbumForCreatingDto.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using BandAPI.ValidationAtributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BandAPI.Dtos
 {
-    public class AlbumForCreatingDto
+    [TitleDescriptionAttribute(ErrorMessage = "The title and description need to be different")]
+    public class AlbumForCreatingDto //:IValidatableObject
     {
+        [Required(ErrorMessage ="Title needs to be filled in")]
+        [MaxLength(200, ErrorMessage ="Title needs to be up to 200 characters")]
         public string Title { get; set; }
+        //[Required]
+        [MaxLength(400, ErrorMessage = "Description needs to be up to 400 characters")]
         public string Description { get; set; }
-       
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //        yield return new ValidationResult("The title and description need to be different", new[] { "AlbumForCreatingDto" });
+        //}
     }
 }
---
+++ BandAPI/ValidationAtributes/TitleDescriptionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using BandAPI.Dtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace BandAPI.ValidationAtributes
+{
+    public class TitleDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var album = (AlbumForCreatingDto)validationContext.ObjectInstance;
+            if (album.Title == album.Description)
+            {
+                return new ValidationResult("The title and description need to be different.", new[] { "AlbumForCreatingDto" });
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
